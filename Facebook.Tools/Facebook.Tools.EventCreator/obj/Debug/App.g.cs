7d38a9d392bc191d52a9c601f8a4dbc88da6bec7
Stan Butler <sbutler@nerdery.com>
2013-06-13 13:20:58 -0500

    Added install project to solution.

--- Facebook.Tools/Facebook.Tools.EventCreator/obj/Debug/App.g.cs
+++ Facebook.Tools/Facebook.Tools.EventCreator/obj/Debug/App.g.cs
@@ -57,7 +57,7 @@ namespace Facebook.Tools.EventCreator {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Facebook.Tools.EventCreator;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/EventCreator;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
